5cffff9c516f0b3ba2ccdd62874de4d4c623ba5e
Ekaterina Vaartis <vaartis@kotobank.ch>
2021-12-10 22:55:13 +0300

    Set a higher interval

--- src/hellish_web/hellish_web-peers.adb
+++ src/hellish_web/hellish_web-peers.adb
@@ -109,7 +109,7 @@ package body Hellish_Web.Peers is
             end;
          end if;
 
-         Result_Map.Include("interval", Encode(30));
+         Result_Map.Include("interval", Encode(60 * 5));
 
          return Encode(Result_Map);
       end Encode_Hash_Peers_Response;

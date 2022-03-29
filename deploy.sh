3d4d5c39467eba09daaf88e60940555dadfb78f3
Ekaterina Vaartis <vaartis@kotobank.ch>
2022-03-29 20:35:20 +0300

    Add deployment build script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+alr build
+
+rm -rf _deploy
+mkdir _deploy _deploy/db/
+cp bin/hellish _deploy/
+
+echo "Copying assets"
+cp -r assets/ _deploy/
+cp aws.ini _deploy/
+cp -r db/migrations/ _deploy/db/migrations/
+
+echo "Done"
